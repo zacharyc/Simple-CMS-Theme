0c3bfd0e3bfec5ba92619e724d617f2272525317
Zachary Cohen <zachary@zacharyc.com>
2009-11-11 10:33:39 -0800

    Moved the date to appear after the post title in single.php

--- single.php
+++ single.php
@@ -1,9 +1,9 @@
 <?php	get_header(); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<span class="date"><?php the_time(__('F jS, Y')) ?></span>
 	
 	<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
+	<span class="date"><?php the_time(__('F jS, Y')) ?></span>
 	<?php the_content('<br /><br />Continue Reading &raquo;'); ?>
 <?php endwhile; ?>
 
